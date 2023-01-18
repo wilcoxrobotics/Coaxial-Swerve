f0368533c0992a575ba9af738082d2632efc92d0
DevMello <kol.kid.py@gmail.com>
2023-01-17 23:40:02 -0800

    added the Robot Class. implemented Arm subsystem in it

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Junctions.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.lib;
+
+public enum Junctions {
+    GROUND,
+    LOW,
+    MEDIUM,
+    HIGH
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/ArmSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/ArmSubsystem.java
@@ -1,17 +1,20 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
 import com.qualcomm.robotcore.hardware.CRServoImplEx;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import org.firstinspires.ftc.teamcode.lib.onbot.DcMotorWrapper;
 
 public class ArmSubsystem {
-    private DcMotorWrapper linSlideLeft;
-    private DcMotorWrapper linSlideRight;
+    private DcMotorEx linSlideLeft;
+    private DcMotorEx linSlideRight;
     protected CRServoImplEx v4bMoveRight;
     protected CRServoImplEx v4bMoveLeft;
     protected CRServoImplEx clawPitch;
     protected CRServoImplEx clawOpen;
     protected CRServoImplEx clawRot;
-    public ArmSubsystem (DcMotorWrapper linSlideLeft,DcMotorWrapper linSlideRight,CRServoImplEx v4bMoveRight, CRServoImplEx v4bMoveLeft, CRServoImplEx clawPitch, CRServoImplEx clawOpen, CRServoImplEx clawRot) {
+    public ArmSubsystem (DcMotorEx linSlideLeft, DcMotorEx linSlideRight, CRServoImplEx v4bMoveRight, CRServoImplEx v4bMoveLeft, CRServoImplEx clawPitch, CRServoImplEx clawOpen, CRServoImplEx clawRot) {
+        this.linSlideLeft = linSlideLeft;
+        this.linSlideRight = linSlideRight;
         this.v4bMoveLeft = v4bMoveLeft;
         this.v4bMoveRight = v4bMoveRight;
         this.clawOpen = clawOpen;
